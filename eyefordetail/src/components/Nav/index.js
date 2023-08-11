b2eb44bac33aa2ee71cdeb366e4e37dd622f920f
Heather Violet <heatherviolet91@gmail.com>
2023-08-11 14:37:06 -0700

    basic title

---
+++ eyefordetail/src/components/Nav/index.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import { Nav } from "react-bootstrap";
+
+function NavBar(props) {
+    const {
+        linkSelected,
+        setLinkSelected,
+      } = props;
+
+  return (
+     <Nav>
+
+<h2 class="text-primary"
+style={{ paddingLeft: '30px'}}>Eye For Detail</h2>
+
+  <Nav.Item>
+  <Nav.Link id="about" href="#about" onClick={() => setLinkSelected("about")}>
+      About Me
+</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+  <Nav.Link id="projects" onClick={() => setLinkSelected("projects")}>
+      Projects
+</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link className={`mx-2 ${linkSelected && 'navActive'}`}>
+            <span onClick={() => setLinkSelected("contact")}>Contact</span>
+            </Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+  <Nav.Link>
+            <span onClick={() => setLinkSelected("resume")}>Resume</span>
+            </Nav.Link>
+  </Nav.Item>
+</Nav>
+  );
+}
+
+export default NavBar;
